43ab1d71586e93b98e3665e243a96c653ecb6eda
ariaworld <143797359+ariaworld@users.noreply.github.com>
2026-08-11 02:18:21 +0200

    UI: Fix research tech nodes UI

--- tgui/packages/tgui/components/Grid.js
+++ tgui/packages/tgui/components/Grid.js
@@ -9,22 +9,15 @@ export const Grid = props => {
     children,
     ...rest
   } = props;
-  const columnEmpty = [];
-  const rowEmpty = [];
-
-  // Some spaghetti code to make -ms-grid work properly
-  for (let i = 1; i <= columns+1; i++) {
-    columnEmpty.push(<Box width={gridSize} height={gridSize} style={{ "-ms-grid-column": i, "-ms-grid-row": 1 }} />);
-  }
-
-  for (let i = 2; i <= rows+1; i++) {
-    rowEmpty.push(<Box width={gridSize} height={gridSize} style={{ "-ms-grid-column": 1, "-ms-grid-row": i }} />);
-  }
 
   return (
-    <Box style={{ "display": "-ms-grid" }} {...rest}>
-      {columnEmpty}
-      {rowEmpty}
+    <Box
+      style={{
+        "display": "grid",
+        "grid-template-columns": `repeat(${columns + 1}, ${gridSize})`,
+        "grid-template-rows": `repeat(${rows + 1}, ${gridSize})`,
+      }}
+      {...rest}>
       {children}
     </Box>
   );
@@ -41,7 +34,13 @@ export const GridItem = props => {
   } = props;
 
   return (
-    <Box style={{ "-ms-grid-column": firstColumn, "-ms-grid-row": firstRow, "-ms-grid-column-span": secondColumn, "-ms-grid-row-span": secondRow }} {...rest}>
+    <Box
+      style={{
+        "position": "relative",
+        "grid-column": `${firstColumn} / span ${Math.max(1, secondColumn || 1)}`,
+        "grid-row": `${firstRow} / span ${Math.max(1, secondRow || 1)}`,
+      }}
+      {...rest}>
       {children}
     </Box>
   );
